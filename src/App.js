9f9e76ee90cb89a8d375d48486d9bee139db0691
pomps74 <44311946+pomps74@users.noreply.github.com>
2019-03-30 21:58:40 -0700

    the shitty version

--- src/App.js
+++ src/App.js
@@ -1,67 +1,65 @@
 import React, { Component } from 'react';
-import { Column, Row } from 'simple-flexbox';
+import { Row } from 'simple-flexbox';
 
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div>
+      <div className="component-app">
         <Row horizontal='center'>
           <h1>LAHacks </h1>
         </Row>
-        
         <Row horizontal='center'>
-
-          <Column flexGrow={1} horizontal='center'>
-            <div style={styles.box}>
-              <span> Blue Shield </span>
-            </div>
-          </Column>
-          
-          <Column flexGrow={1} horizontal='center'>
-            <div style={styles.box}>
-              <span> Dignity Health </span>
-            </div>
-          </Column>
-
-          <Column flexGrow={1} horizontal='center'>
-            <div style={styles.box}>
-              <span> Kaiser Permanente </span>
-            </div>
-          </Column>
+          <h2>Please select your organization: </h2>
         </Row>
-        
-        </div>
+
+        <Row flexGrow={1} horizontal='space-between'>
+          <button style={styles.box}>
+            <span> Blue Shield </span>
+          </button>
+
+          <button style={styles.box}>
+            <span> Dignity Health </span>
+          </button>
+
+          <button style={styles.box}>
+            <span> Kaiser Permanente </span>
+          </button>
+        </Row>
+        <Row flexGrow={1} horizontal='space-between'>
+          <button style={styles.box}>
+            <span> Mercy Hospital </span>
+          </button>
+
+          <button style={styles.box}>
+            <span> UCI Health </span>
+          </button>
+
+          <button style={styles.box}>
+            <span> UCLA Health </span>
+          </button>
+        </Row>
+        <Row horizontal='flex-start'>
+          <button style={styles.box}>
+            <span> UCSD Health </span>
+          </button>
+        </Row>
+
+      </div>
     );
   }
 }
+
 export default App;
 
 const styles = {
   box: {
     flex: 1,
     padding: 20,
-    backgroundColor: '#f4f4f4',
+    //borderColor: 'white',
+    backgroundColor: '#FFF',
     boxShadow: '0px 4px 4px rgba(0, 0, 0, 0.25)',
     borderRadius: 20
   },
-  boxWithShadow: {
-    flex: 1,
-    flexDirection: 'row',
-    backgroundColor: 'white',
-    justifyContent: 'space-between',
-    borderRadius: 20,
-    padding: 20,
-    margin: 10,
-    shadowColor: '#000',
-    shadowOffset: { width: 0, height: 4 },
-    shadowOpacity: 0.25,
-    shadowRadius: 1.5,
-    elevation: 4
-  },
-  boxIcon: {
-    position: 'relative',
-    bottom: 5,
-  }
 }
